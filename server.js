ec2f520e6987030caa320c1af5d367362df6536c
ktp242 <ktp242@nyu.edu>
2013-11-02 21:20:47 -0400

    20131102 v.01

---
+++ server.js
@@ -0,0 +1,22 @@
+var http = require('http');
+var fs = require('fs'); // Using the filesystem module
+var httpServer = http.createServer(requestHandler);
+httpServer.listen(8080);
+
+function requestHandler(req, res) {
+	// Read index.html
+	fs.readFile(__dirname + '/filterTest.html', 
+		// Callback function for reading
+		function (err, data) {
+			// if there is an error
+			if (err) {
+				res.writeHead(500);
+				return res.end('Error loading index.html');
+			}
+			// Otherwise, send the data, the contents of the file
+			res.writeHead(200);
+			res.end(data);
+			console.log("server connected")
+  		}
+  	);
+}
